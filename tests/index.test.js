dca2791528766696c09599b84b60fc9c5cbc0bff
Sacha Clerc-Renaud <sachacr@gmail.com>
2018-06-10 22:40:57 +0200

    Some changes

--- tests/index.test.js
+++ tests/index.test.js
@@ -13,21 +13,18 @@ describe('koa-router-map', function () {
 
   describe('Table mode', function () {
     it('Should return routes data in table mode', async () => {
-      const res = await superagent.get('http://localhost:9000/routes/map?color=true')
-console.log(res.text)
-
-      const expectedValue = `╔═══════════╤═══════════════╤═══════════════════╤═════════════════╗
-║ Methods   │ Path          │ Name              │ Stack           ║
-╟───────────┼───────────────┼───────────────────┼─────────────────╢
-║ HEAD, GET │ /examples     │ Get all Examples  │ examplesHandler ║
-╟───────────┼───────────────┼───────────────────┼─────────────────╢
-║ POST      │ /examples     │ Create examples   │ examplesHandler ║
-╟───────────┼───────────────┼───────────────────┼─────────────────╢
-║ DELETE    │ /examples/:id │ Delete examples   │ examplesHandler ║
-╟───────────┼───────────────┼───────────────────┼─────────────────╢
-║ HEAD, GET │ /examples/:id │ Get example by id │ examplesHandler ║
-╚═══════════╧═══════════════╧═══════════════════╧═════════════════╝`
-
+      const res = await superagent.get('http://localhost:9000/routes/map')
+      const expectedValue = `╔═══════════╤═══════════════╤═══════════════════╤═══════════════════╗
+║ Methods   │ Path          │ Name              │ Stack             ║
+╟───────────┼───────────────┼───────────────────┼───────────────────╢
+║ HEAD, GET │ /examples     │ Get all Examples  │ getAllExamples    ║
+╟───────────┼───────────────┼───────────────────┼───────────────────╢
+║ POST      │ /examples     │ Create examples   │ createExample     ║
+╟───────────┼───────────────┼───────────────────┼───────────────────╢
+║ DELETE    │ /examples/:id │ Delete examples   │ deleteExampleById ║
+╟───────────┼───────────────┼───────────────────┼───────────────────╢
+║ HEAD, GET │ /examples/:id │ Get example by id │ getExampleById    ║
+╚═══════════╧═══════════════╧═══════════════════╧═══════════════════╝`
       assert.equal(res.text, expectedValue)
       assert.equal(res.status, 200)
     })
@@ -36,16 +33,20 @@ console.log(res.text)
   describe('Raw mode', function () {
     it('Should return routes data in raw mode', async () => {
       const res = await superagent.get('http://localhost:9000/routes/map?raw=true')
-      console.log(res.text)
-
       const expectedValue = `Methods | Path | Name | Stack
-HEAD, GET | /examples | Get all Examples | examplesHandler
-POST | /examples | Create examples | examplesHandler
-DELETE | /examples/:id | Delete examples | examplesHandler
-HEAD, GET | /examples/:id | Get example by id | examplesHandler`
-
+HEAD, GET | /examples | Get all Examples | getAllExamples
+POST | /examples | Create examples | createExample
+DELETE | /examples/:id | Delete examples | deleteExampleById
+HEAD, GET | /examples/:id | Get example by id | getExampleById`
       assert.equal(res.text, expectedValue)
       assert.equal(res.status, 200)
+    })
+  })
+
+  describe('General', function () {
+    it('Should not intercept others request than /routes/map', async () => {
+      const res = await superagent.get('http://localhost:9000/test').catch(err => err.response)
+      assert.equal(res.status, 404)
     })
   })
 })
